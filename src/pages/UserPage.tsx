1a77c2e12eaa45967019fc844f8f163ebbcad055
= <moonkey48@icloud.com>
2022-12-04 22:11:39 +0900

    delete Main

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -2,15 +2,18 @@ import React, { Suspense } from 'react';
 import fetchData from '../api/fetchData';
 import Loading from '../components/Loading';
 import User from '../components/User';
+import Header from '../components/Header';
 
 const UserPage = () => {
     return (
+        <>
+        <Header/>
         <main>
-            <h1>welcome user. </h1>
             <Suspense fallback={<Loading loadDataName='user'/>}>
                 <User resource={fetchData('1')} />
             </Suspense>
         </main>
+        </>
     )
 }
 
